78b9434e2396ee83317951536c4003718f5a2e68
RedCatik <48980988+RedCatik@users.noreply.github.com>
2019-03-29 10:24:24 +0300

    Add files via upload

--- Monkey/Monkey.py
+++ Monkey/Monkey.py
@@ -7,26 +7,31 @@ import msvcrt
 from threading import Thread
 import threading
 
+path = os.getcwd()
 clear = lambda: os.system('cls') #функция, которая очищает экран
-snorepath = "C:\\Kharitonov\\snore.wav"
-songpath = "C:\\Kharitonov\\mix.wav"
+snorepath = path+"\\snore.wav"
+songpath = path+"\\mix.wav"
 
-def morgat():
+def morgat():	
 	q = "   /---\\ " # Все буквы - переменные
 	x = "  /     \\"
 	z = "  |  O  |"
 	w = "  \\     /"
 	a = "   \\___/ "
 	y = "   -----"
-	for i in range(2):
-		print (q, q)
+	f = "_______"
+	g = "\\_____/"
+	for i in range(10): #пока правда:
+		print ( q,  q)
 		print (x, x)
 		print (z, z)
 		print (w, w)
-		print (a, a)
+		print (a,  a)
+		print ("\n\n       ",g)
 		time.sleep(2) #время остановки
 		clear() #вызов функции 
 		print ("\n\n",y, y)
+		print ("\n\n       ",f)
 		time.sleep(0.5) #время остановки
 		clear() #вызов функции
 
@@ -46,21 +51,25 @@ class Monkey:
 		if self.sleep >= 100:
 			self.sleep = 100
 	def Wake(self):
-		print("Накликайте 30 раз чтобы разбудить спящую обезбяну")
-		start = time.time()
-		kolvo = []
-		PERIOD_OF_TIME = 5
-		while True :
-			if time.time() > start + PERIOD_OF_TIME : break
-			ret = msvcrt.getch()
-			kolvo.append(ret)
+		while True:
+			print("Накликайте 30 раз чтобы разбудить спящую обезбяну")
+			start = time.time()
+			kolvo = []
+			PERIOD_OF_TIME = 5
 
-		print(len(kolvo))
-		if len(kolvo)>30:
-			print("Вы разбудили древнее зло")
-		winsound.PlaySound(None, winsound.SND_PURGE)
-		winsound.PlaySound(songpath, winsound.SND_ASYNC)
-		morgat()
+			while True :
+				if time.time() > start + PERIOD_OF_TIME : break
+				ret = msvcrt.getch()
+				kolvo.append(ret)
+
+			print(len(kolvo))
+
+			if len(kolvo)>30:
+				print("Вы разбудили древнее зло")
+				winsound.PlaySound(None, winsound.SND_PURGE)
+				winsound.PlaySound(songpath, winsound.SND_ASYNC)
+				morgat()
+				break
 		self.issleep = False
 	def Eat(self):
 		print(self.name + " ест")
@@ -68,19 +77,18 @@ class Monkey:
 		pass
 
 e = Monkey("Ежан", 100, 100)
-
+stroka = "" # Создаём переменную
 while True:  # Начинаем цикл
-	stroka = input()  # Создаём переменную
 	if stroka == ("Спи, " + e.name) and not e.issleep:
 		e.Sleep()
-	elif stroka == (e.name+" просыпайся") and e.issleep:
+	elif stroka == (e.name+", просыпайся") and e.issleep:
 		e.Wake()
 	else: # Если содержание переменной не совпадает с условием то...
 		if e.issleep:
 			e.Sleep()
 		else:
-			e.Wake()
-	
+			e.Sing()
+	stroka = input()
 
 while True:
 	if e.issleep:
